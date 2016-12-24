515d46d25d02887cc84145b581f3eacd4b3c73bf
Keith Johnson <keithyjohnson@gmail.com>
2016-12-23 18:20:31 -0600

    fitting the training data

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 train = pd.read_csv("cleanedlabledTrainData.tsv", header=0, quoting=3)
 
@@ -10,3 +11,10 @@ print(train["review"][:5])
 train_data_features = vectorizer.fit_transform(train['review'])
 vocab = vectorizer.get_feature_names()
 print('sampling vocab: ', vocab[0:20])
+
+forest = RandomForestClassifier(n_estimators = 100)
+
+# Fit the forest to the training set, using the bag of words as
+# features and the sentiment labels as the response variable
+forest = forest.fit( train_data_features, train["sentiment"] )
+
