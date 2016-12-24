ceaca93e17dc7fd7c29789e44820f53fc7e8faa7
Keith Johnson <keithyjohnson@gmail.com>
2016-12-23 18:19:35 -0600

    Using CountVectorizer to fit the model fit the model and transform the data into feature vectors

--- run.py
+++ run.py
@@ -6,3 +6,7 @@ print('train.shape: ', train.shape)
 print('train.columns.values: ', train.columns.values)
 print('sampling first five reviews from {} total reviews'.format(train['review'].size))
 print(train["review"][:5])
+
+train_data_features = vectorizer.fit_transform(train['review'])
+vocab = vectorizer.get_feature_names()
+print('sampling vocab: ', vocab[0:20])
