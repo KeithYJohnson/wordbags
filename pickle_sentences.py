de50aeeb264d5c21726533d324fbfa3f5f1792ca
Keith Johnson <keithyjohnson@gmail.com>
2016-12-25 17:50:28 -0600

    fixing pickle_sentence bug

--- pickle_sentences.py
+++ pickle_sentences.py
@@ -5,17 +5,20 @@ import nltk.data
 from six.moves import cPickle as pickle
 
 labeled_train = pd.read_csv('cleaned_rmswFalse_rmnlFalselabeledTrainData.tsv', header=0)
-unlabled_tarin = pd.read_csv('cleaned_rmswFalse_rmnlFalseunlabeledTrainData.tsv', header=0)
+unlabeled_train = pd.read_csv('cleaned_rmswFalse_rmnlFalseunlabeledTrainData.tsv', header=0)
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 sentences = []
 
-print("Parsing sentences from training set")
-for review in labeled_train["review"]:
+print('sentencing {} labeled examples'.format(labeled_train.shape))
+for idx, review in enumerate(labeled_train["review"]):
+    print('labeled: ', idx)
     sentences += review_to_sentences(review, tokenizer)
 
 print("Parsing sentences from unlabeled set")
-for review in unlabeled_train["review"]:
+print('sentencing {} unlabeled examples'.format(unlabeled_train.shape))
+for idx, review in enumerate(unlabeled_train["review"]):
+    print('unlabeled: ', idx)
     sentences += review_to_sentences(review, tokenizer)
 
 f = open(SENTENCES_PICKLE_FILE, 'wb')
