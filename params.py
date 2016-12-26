7d5464a8dbbfc84f30774624dfbb33df1dab0b94
Keith Johnson <keithyjohnson@gmail.com>
2016-12-26 12:05:24 -0600

    some extra params

--- params.py
+++ params.py
@@ -3,6 +3,10 @@ FOREST_PICKLE_FILE = 'forest.pickle'
 PREDICTION_PICKLE_FILE = 'results.pickle'
 CLEAN_TEST_DATA = 'cleaned_testData.tsv'
 SENTENCES_PICKLE_FILE = 'sentences.pickle'
+WORD2VEC_MODEL = "300features_40minwords_10context"
+KMEANS_PICKLE = 'kmeans.pickle'
+KMEANS_DICT = 'kmeans_dict.pickle'
+CENTROIDS_BAG = 'centroids_bag_model.pickle'
 
 vectorizer = CountVectorizer(
     analyzer = "word",
